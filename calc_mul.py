e5f8c55e01da8e404fa71fb7b1e6f7e16414ff82
mikan <nishi.genki.lo@tut.jp>
2025-02-11 20:43:22 +0900

    add three files

--- calc_mul.py
+++ calc_mul.py
@@ -3,8 +3,39 @@
 import re
                 
 def calc(A,B):
-        ai=str(A)
-        bi=str(B)
+
+        #A,Bが整数ではない場合
+        if(type(A) == type(1) and type(B) == type(1)):
+                try:
+                        ai=str(A)
+                        bi=str(B)
+
+                        p = re.compile('\d+$')                 #整数のみ受理可能に
+                        if p.match(ai) or p.match(bi):
+                                a=int(ai)                       #castの型をfloatからintへ
+                                b=int(bi)
+
+                        #条件を0<a<b<1000から，1<=a<=999と1<=b<=999へ変更
+                                if (1<=a and a<=999) and (1<=b and b<=999):      
+                                        valid=True
+                                else:
+                                        valid=False
+                        else:
+                                valid=False
+                        
+                        if valid:
+                                ans=a*b
+                                return ans
+                        else:
+                                return -1        
+                except:
+                        return -1
+        else:
+                return -1
+
+
+#変更前
+"""        
         p = re.compile('\d+(\.\d+)?')
         if p.match(ai) or p.match(bi):
                 a=float(ai)
@@ -21,7 +52,9 @@ def calc(A,B):
                 return ans
         else:
                 return -1
-        
+"""
+
+
                 
 def main ():
 	matchstring = ''
--- test_cases.py
+++ test_cases.py
@@ -2,6 +2,7 @@
 
 import unittest
 from calc_mul import calc
+import sys
 
 # Run with testrunner so needs to be in file test_
 
@@ -19,3 +20,21 @@ class TestCalc (unittest.TestCase):
         def test_sample4 (self):
                 self.assertEqual (-1, calc(0.1,999))
 
+        #追加分
+
+        def test_sample5 (self):
+                self.assertEqual (-1, calc(1000,150))
+
+        def test_sample6 (self):
+                self.assertEqual (150, calc(1,150))
+
+        def test_sample7 (self):
+                self.assertEqual (149850, calc(999,150))
+
+        def test_sample8 (self):
+                self.assertEqual (-1, calc(sys.maxsize,sys.maxsize))
+
+        def test_sample9 (self):
+                self.assertEqual (-1, calc("1","100"))
+        
+        
